155978e97ca94ccaae911ba6d8bcc8e164a85ff6
임광훈 <lgh82229047@gmail.com>
2021-02-18 10:27:41 +0900

    210218 swea

--- 4843_특별한_정렬/sol1.py
+++ 4843_특별한_정렬/sol1.py
@@ -11,10 +11,13 @@ for tc in range(1, T+1):
     for i in range(len(arr) - 1):
         temp = i
         for j in range(i + 1, len(arr)):
+            # 홀수면 큰걸 삽입
             if i%2:
                 if arr[j] < arr[temp] : temp = j
                 continue
+            #짝수면 작은걸로
             if arr[j] > arr[temp]: temp = j
+        # 스왑
         arr[i], arr[temp] = arr[temp], arr[i]
 
     result = ' '.join(map(str,arr[:10]))
