cec2e29375e7f8f5071fd8921e6c75c77963afe7
임광훈 <lgh82229047@gmail.com>
2021-02-22 16:55:54 +0900

    승자예측

--- 3456_승자예측/sol1.py
+++ 3456_승자예측/sol1.py
@@ -10,22 +10,14 @@ for tc in range(1, T+1):
     k = 1
     winner = ['Alice', 'Bob']
 
-    while n > 9:
-        n = n / 2 + k
+    while n > 2:
+        n = n // 2 + k
         k = -k
 
-    if n == 1 or 6 <= n <= 9:
+    if n == 1:
         i = 1
     else:
         i = 0
-
-    if int(n) == n:
-        print('ㅇㅇ')
-        pass
-
-    # else:
-    #     if i: i = 0
-    #     else: i = 1
 
     print("#{} {}".format(tc, winner[i]))
 
