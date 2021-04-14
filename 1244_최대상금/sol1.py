00bc5285c520dc9ff3482be3aa41564b4b9b1d64
임광훈 <lgh82229047@gmail.com>
2021-04-14 13:28:16 +0900

    idk, but changed

--- 1244_최대상금/sol1.py
+++ 1244_최대상금/sol1.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin = open("input.txt")
+
 def reward(M, count, k):
     global maxi, size, ch_cnt
     if count == N:
--- 5185/sol1.py
+++ 5185/sol1.py
@@ -4,6 +4,16 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 dict16 = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
     'A': '1010',
     'B': '1011',
     'C': '1100',
@@ -16,17 +26,7 @@ for tc in range(1, T+1):
     n, inp = input().split()
     temp = ''
     for i in range(int(n)):
-        if inp[i] in dict16.keys():
-            temp += dict16[inp[i]]
-        else:
-            m = int(inp[i])
-            new_m = ''
-            while m:
-                new_m = str(m % 2) + new_m
-                m //= 2
-            if len(new_m) < 4:
-                new_m = '0'*(4-len(new_m))+new_m
-            temp += new_m
+        temp += dict16[inp[i]]
 
     print("#{} {}".format(tc, temp))
 
