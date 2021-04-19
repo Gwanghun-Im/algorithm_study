85a9aa788fccd05d13c09c4f504bc32694ed702b
임광훈 <lgh82229047@gmail.com>
2021-04-19 17:37:37 +0900

    5208 swea:

--- 5208_전기버스2/sol1.py
+++ 5208_전기버스2/sol1.py
@@ -4,6 +4,26 @@ sys.stdin = open("sample_input.txt")
 T = int(input())
 
 
+def solve(idx, now):
+    global cnt
+    if now >= cnt:
+        return
+
+    if idx >= n-1:
+        if now<cnt:
+            cnt = now
+        return
+
+    for i in range(charger[idx]):
+        solve(idx + i+1, now+1)
+
+
+
 for tc in range(1, T+1):
-    
-    print("#{} ".format(tc, ))
+    inp = list(map(int, input().split()))
+    n = inp[0]
+    charger = inp[1:]
+    cnt = n
+    solve(0, -1)
+
+    print("#{} {}".format(tc, cnt))
