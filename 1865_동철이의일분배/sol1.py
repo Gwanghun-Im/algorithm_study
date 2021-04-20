c6ce05b3902c7e3bf3c15a88d9bbd71292bbdce3
임광훈 <lgh82229047@gmail.com>
2021-04-20 13:50:10 +0900

    1865 swea modified

--- 1865_동철이의일분배/sol1.py
+++ 1865_동철이의일분배/sol1.py
@@ -3,8 +3,9 @@ sys.stdin = open("input.txt")
 
 T = int(input())
 
+
 def solve(idx, p):
-    global prop,n
+    global prop, n
 
     if p <= prop:
         return
@@ -16,14 +17,15 @@ def solve(idx, p):
     for i in range(n):
         if not v[i]:
             v[i] = 1
-            solve(idx+1, p*P[idx][i]/100)
+            solve(idx + 1, p * P[idx][i] / 100)
             v[i] = 0
 
-for tc in range(1, T+1):
+
+for tc in range(1, T + 1):
     n = int(input())
     P = [list(map(int, input().split())) for _ in range(n)]
-    v = [0]*n
+    v = [0] * n
     prop = 0
-    solve(0,1)
-    print("#{} {:6f}".format(tc, prop*100))
+    solve(0, 1)
+    print("#{} {:6f}".format(tc, prop * 100))
 
